b7f2f90491a2a87cc4d18bbb801450b45e8e2d78
Unknown <tatan.alegria@hotmail.com>
2019-05-10 00:24:47 -0400

    Primera version de estructura angular

--- app/directives.js
+++ app/directives.js
@@ -2,22 +2,6 @@
 
 (function () {
   var direc = angular.module('directives-example',[]);
-
-  direc.directive('miPanel1',function(){
-    return {
-      restric : 'A',
-      templateUrl : 'app/views/panel1.html'
-      
-    }
-  });
-  
-  direc.directive('miPanel2',function(){
-    return {
-      restric : 'E',
-      templateUrl : 'app/views/panel2.html'
-      
-    }
-  }); 
   direc.directive('navbar', function () {
     return {
       restrict: 'E',
