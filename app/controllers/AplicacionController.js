f42214a08fd429819b38173bc656f961217566e0
Misael <m.ramirez11@ufromail.cl>
2019-05-11 00:21:21 -0400

    Pantalla 2 con bd
    Se conectó la pantalla 2 con los datos desde la bd

--- app/controllers/AplicacionController.js
+++ app/controllers/AplicacionController.js
@@ -6,14 +6,14 @@
   // una vez importado los módulos de angular en nuestra variable creamos el código que contendrá el controlador
   // mencionado en index.html y este debe recibir por parametro al $scope para poder utilizarlo y lograr hacer el
   // bind entre las variables del archivo javascript hacia el DOM
-  app.controller('applicationController', ['$scope', function($scope){
-    $scope.ejemplo='Esto es un ejemplo de Angular JSS';
+  app.controller('applicationController', ['$scope','$http',function($scope, $http){
+    $scope.ejemplo='Esto es un ejemplo de Angular JS';
     
 
     //Desarrollamos funciones que nos permitirán lograr una interacción con un tab de bootstrap
     //con un ng-include de angular
     //$scope.menu='app/views/figura2.html';
-    $scope.menu='app/views/figura2.html';
+    $scope.menu='app/views/figura3.html';
     $scope.cambioVista= function (menu) {//funcion que cambia vistas
 
       $scope.menu = 'app/views/'+menu+'.html';//cambio la vista cambiando la ruta de la asociacion
@@ -22,10 +22,22 @@
     $scope.isMenu= function (menu) {
       return $scope.menu==='app/views/'+menu+'.html';
     };
+
+     // http facilita la comunicación con remotos, hace una solcitud al servidor y espera una respuesta
+     $http({
+      method: 'GET',
+      url: '././lib/js/getBooks.php'
+    }).then(function successCallback(response) {
+        // Aqui va todo lo que hacemos cuando logramos la conexion
+        $scope.books = response.data;
+        console.log($scope.books)
+      });
     
     
   }]);
-  app.controller('booksController', ['$scope', '$http',booksController])
+  
+  // Controlador antiguo que se encargaba de cargar los books
+  /*app.controller('booksController', ['$scope', '$http',booksController])
   function booksController($scope, $http){
     // De prueba, BORRAR AL FINAL
     $scope.hola="assd"
@@ -39,5 +51,5 @@
           $scope.books = response.data;
           console.log($scope.books)
         });
-}
+}*/
 })();
